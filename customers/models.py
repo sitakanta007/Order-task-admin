3c5c66e813beb40e298e8baba5699004cdb288ad
Sitakanta <pintu.sitakanta@gmail.com>
2025-11-07 16:12:54 +0530

    bcrypt hash added for passwords

--- customers/models.py
+++ customers/models.py
@@ -1,4 +1,5 @@
 import uuid
+import bcrypt
 from django.db import models
 
 class Customer(models.Model):
@@ -7,6 +8,13 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)   # Store bcrypt hash created by NodeJS
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # If the password looks like raw text (not bcrypt hashed)
+        if not self.password.startswith("$2b$"):
+            # Hash using bcrypt
+            self.password = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.email
